420beeabf3d95b660c63b842a0de56f513333127
Ihor-Voroniuk <88936911+Ihor-Voroniuk@users.noreply.github.com>
2022-08-23 14:08:13 +0300

    reload

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,43 @@
-// Add imports above this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 import { galleryItems } from './gallery-items';
-// Change code below this line
 
-console.log(galleryItems);
+const parentDivEl = document.querySelector('.gallery');
+
+const gallery = galleryItems
+  .map(({ preview, original, description }) => {
+    return `<a class="gallery__link" href="${original}">
+      <img
+      class="gallery__image"
+      src="${preview}"
+      alt="${description}"
+      />
+      </a>`;
+  })
+  .join('');
+
+parentDivEl.insertAdjacentHTML('beforeend', gallery);
+
+new SimpleLightbox('.gallery a', {
+  showCounter: false,
+  captions: true,
+  captionDelay: 250,
+  captionSelector: 'img',
+  captionType: 'attr',
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+});
+
+// ! Задание 1 - библиотека SimpleLightbox
+// * Выполняй это задание в файлах 01 - gallery.html и 01 - gallery.js.
+// * Разбей его на несколько подзадач:
+
+// * 1. Добавь библиотеку SimpleLightbox как зависимость проекта используя
+// * 	npm(ссылка на CDN из твоей прошлой работы больше не нужна).
+
+// * 2. Используй свой JavaScript код из предыдущей домашней работы,
+// * 	но выполни рефакторинг с учетом того, что библиотека была установлена через
+// * 	npm(синтаксис import /export).
+
+// * Для того чтобы подключить CSS код библиотеки в проект, необходимо добавить еще один импорт,
+// * кроме того который описан в документации.
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,78 @@
+import throttle from 'lodash.throttle';
 
+const formFeedbackEl = document.querySelector('.feedback-form');
+const emailInputEl = document.querySelector('[name="email"]');
+const messageInputEl = document.querySelector('[name="message"]');
+
+const KEY_LOCAL_STORAGE = 'feedback-form-state';
+
+const dataInLocalStorage = JSON.parse(localStorage.getItem(KEY_LOCAL_STORAGE));
+
+if (dataInLocalStorage !== null) {
+  emailInputEl.value = dataInLocalStorage.email;
+  messageInputEl.value = dataInLocalStorage.message;
+}
+
+const feedbackFormObj = {
+  email: 'email',
+  message: 'message',
+};
+
+formFeedbackEl.addEventListener(
+  'input',
+  throttle(() => {
+    entryLocalStorage();
+  }, 500),
+);
+
+function entryLocalStorage() {
+  feedbackFormObj.email = emailInputEl.value;
+  feedbackFormObj.message = messageInputEl.value;
+  localStorage.setItem(KEY_LOCAL_STORAGE, JSON.stringify(feedbackFormObj));
+}
+
+formFeedbackEl.addEventListener('submit', e => {
+  e.preventDefault();
+
+  if (emailInputEl.value !== '' && messageInputEl.value !== '') {
+    console.log(feedbackFormObj);
+
+    formFeedbackEl.reset();
+    localStorage.removeItem(KEY_LOCAL_STORAGE);
+  }
+});
+
+// ! Задание 3 - форма обратной связи
+// * В HTML есть разметка формы.Напиши скрипт который будет сохранять значения полей
+// * в локальное хранилище когда пользователь что - то печатает.
+
+// * <form class="feedback-form" autocomplete="off">
+// *   <label>
+// *     Email
+// *     <input type="email" name="email" autofocus />
+// *   </label>
+// *   <label>
+// *     Message
+// *     <textarea name="message" rows="8"></textarea>
+// *   </label>
+// *   <button type="submit">Submit</button>
+// * </form>
+
+// * Выполняй это задание в файлах 03-feedback.html и 03-feedback.js.
+// * Разбей его на несколько подзадач:
+
+// * 1. Отслеживай на форме событие input, и каждый раз записывай в
+// *    локальное хранилище объект с полями email и password,
+// *    в которых сохраняй текущие значения полей формы.
+// *  	Пусть ключом для хранилища будет строка "feedback-form-state".
+
+// * 2. При загрузке страницы проверяй состояние хранилища,
+// *    и если там есть сохраненные данные, заполняй ими поля формы.
+// *    В противном случае поля должны быть пустыми.
+
+// * 3. При сабмите формы очищай хранилище и поля формы,
+// *    а также выводи объект с полями email,
+// *    password и текущими их значениями в консоль.
+
+// * 5. Сделай так, чтобы хранилище обновлялось не чаще чем раз в 500 миллисекунд.
+// *    Для этого добавь в проект и используй библиотеку lodash.throttle.
